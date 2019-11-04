1c149956caec6474c7f35a9b9c3924356864938b
BenHall-7 <benjaminjahall@gmail.com>
2019-11-04 01:02:20 -0500

    add an animation, and events 6-8

--- js/index.js
+++ js/index.js
@@ -17,13 +17,13 @@ img1.addEventListener("mouseover", switch_img_src);
 img2.addEventListener("mouseover", switch_img_src);
 
 // 2: scroll
-document.addEventListener("scroll", (ev) => {
+document.addEventListener("scroll", ev => {
     document.querySelector("body").style.transform = "rotate("+scrollY / 180+"deg)";
     ev.stopPropagation();
 })
 
 // 3: click
-document.querySelectorAll(".nav-link").forEach((nav_link) => nav_link.addEventListener("click", (ev) => {
+document.querySelectorAll(".nav-link").forEach(nav_link => nav_link.addEventListener("click", (ev) => {
     nav.removeChild(nav_link);
     ev.preventDefault();
 }))
@@ -37,3 +37,31 @@ window.addEventListener("load", () => {
 document.querySelectorAll("p").forEach(p => p.addEventListener("dblclick", ev => {
     ev.target.style.background = "orange"
 }));
+
+// 6: resize
+window.addEventListener("resize", () => {
+    document.querySelector("body").replaceWith(
+        "Resizing is not allowed here. You must reload the page."
+    )
+});
+
+document.querySelectorAll("img").forEach(img => {
+    img.style.animation = "shake";
+    img.style.animationDuration = "0.25s";
+    img.style.animationPlayState = "paused";
+    img.style.animationIterationCount = "infinite";
+})
+
+// 7: keydown
+document.addEventListener("keydown", () => {
+    document.querySelectorAll("img").forEach(img => {
+        img.style.animationPlayState = "running";
+    })
+})
+
+// 8: keyup
+document.addEventListener("keyup", () => {
+    document.querySelectorAll("img").forEach(img => {
+        img.style.animationPlayState = "paused";
+    })
+})
